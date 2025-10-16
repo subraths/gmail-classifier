91effb44fbed941fd55ce7a4a19edbfda341d991
Subrath S <subrath0626@gmail.com>
2025-10-16 12:39:30 +0530

    clean login logic

--- src/app/api/auth/google/callback/route.ts
+++ src/app/api/auth/google/callback/route.ts
@@ -1,36 +1,56 @@
 import path from 'path';
 import process from 'process';
 import { google } from 'googleapis';
-import { readFile } from 'fs/promises';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
-export async function GET(req: NextRequest) {
+export async function GET(req: NextRequest, response: NextResponse) {
   const url = new URL(req.url);
   const code = url.searchParams.get('code');
 
-  const fileBuffer = await readFile(CREDENTIALS_PATH);
-  const file = JSON.parse(fileBuffer.toString());
-
-  const { client_secret, client_id, redirect_uris } = file.web;
+  if (!code) {
+    return new Response('Missing code', { status: 400 });
+  }
 
   const oauth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris,
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI,
   );
 
-  const res = await oauth2Client.getToken(code || '');
-  console.log('res from google: ', res);
+  try {
+    const { tokens } = await oauth2Client.getToken(code);
 
-  const payload = res.tokens;
-  const html = `
-    <script>
-      window.opener.postMessage(${JSON.stringify(payload)}, window.origin);
-      window.close();
-    </script>
-  `;
-  return new Response(html, { headers: { 'Content-Type': 'text/html' } });
+    if (!tokens) {
+      return new Response('Error retrieving access token', { status: 500 });
+    }
+
+    const cookie = await cookies();
+    cookie.set('access_token', tokens.access_token ?? '', { httpOnly: true });
+    cookie.set('refresh_token', tokens.refresh_token ?? '', {
+      httpOnly: true,
+    });
+    cookie.set('token_type', tokens.token_type ?? '', { httpOnly: true });
+    cookie.set('expiry_date', tokens.expiry_date?.toString() ?? '', {
+      httpOnly: true,
+    });
+    cookie.set('id_token', tokens.id_token ?? '', { httpOnly: true });
+    cookie.set('scope', tokens.scope ?? '', { httpOnly: true });
+
+    const html = `
+      <html>
+        <script>
+          window.close();
+        </script>
+      </html>
+    `;
+
+    return new Response(html, { headers: { 'Content-Type': 'text/html' } });
+  } catch (error) {
+    console.log(error);
+    return new Response('Error retrieving access token', { status: 500 });
+  }
 }
--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,22 +1,20 @@
 import path from 'path';
 import process from 'process';
 import { google } from 'googleapis';
-import { readFile } from 'fs/promises';
 import { NextRequest } from 'next/server';
 
-const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+const SCOPES = [
+  'https://www.googleapis.com/auth/gmail.readonly',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile',
+];
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
 export async function GET(req: NextRequest) {
-  const fileBuffer = await readFile(CREDENTIALS_PATH);
-  const file = JSON.parse(fileBuffer.toString());
-
-  const { client_secret, client_id, redirect_uris } = file.web;
-
   const oauth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris,
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI,
   );
 
   const url = oauth2Client.generateAuthUrl({
