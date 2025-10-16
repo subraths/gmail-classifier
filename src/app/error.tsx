ad39305c478e422e1a3f77131d6529922ae58cc5
Subrath S <subrath0626@gmail.com>
2025-10-16 12:41:42 +0530

    errors page as a fallback for unhandled errors

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Error() {
+  return (
+    <div className="rounded-xl bg-red-300 px-4 py-2">
+      <p>Something went wrong. Please try again later.</p>
+    </div>
+  );
+}
